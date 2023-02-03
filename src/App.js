90252d1447b3153d3fdd09e27caeb270929539ea
PJMcKinney <pj.mckinney@zinkworks.com>
2023-02-03 15:51:08 +0000

    message

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { Stack } from "@mui/system";
-import { Typography } from "@mui/material";
+import { Stack, Box } from "@mui/system";
+import { Typography, Card } from "@mui/material";
 
 function App() {
-  return <Stack>
-    <Typography variant="h1" component="h2">
-      Our Website
-    </Typography>
-
-
-
-  </Stack>
+  return (
+    <Stack>
+      <Typography variant="h1" component="h2">
+        Our Website
+      </Typography>
+      <Box>
+        <Card variant="outlined">
+          <Typography variant="h3">Team Members:</Typography>
+          <Box paddingLeft="50px">
+            <Typography variant="h4">Adrian</Typography>
+            <Typography variant="h4">Agata</Typography>
+            <Typography variant="h4">Kyrsti</Typography>
+            <Typography variant="h4">Patrick</Typography>
+            <Typography variant="h4">PJ</Typography>
+            <Typography variant="h4">Wojciech</Typography>
+          </Box>
+        </Card>
+      </Box>
+    </Stack>
+  );
 }
 
 export default App;
